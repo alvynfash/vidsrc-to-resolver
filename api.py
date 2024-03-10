41012298ed518a2484445761e4fc2f9625730d8c
Alvyn Fasuyi <zetagik@gmail.com>
2024-03-10 10:30:28 +0100

    Revert "test static"
    This reverts commit 13cf64b1cb62f5a8404b2154b22e7c35293fe1fe.

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 from vidsrc import VidSrcExtractor
 from utils import NoSourcesFound
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 
 # Health route
 @app.route('/health', methods=['GET'])
