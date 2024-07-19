9da8003e615dc488eb5d29d6e56322f80798893a
Alvyn Fasuyi <zetagik@gmail.com>
2024-07-19 03:13:03 +0200

    added more trackers

--- static/js/TMDB.js
+++ static/js/TMDB.js
@@ -44,7 +44,7 @@ function fetch(url) {
 
 function storeTorrentHash(hash) {
     return new Promise(function (resolve, reject) {
-        const data = `magnet:?xt=urn:btih:${hash}`;
+        const data = `magnet:?xt=urn:btih:${hash}&tr=udp://open.demonii.com:1337/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://p4p.arenabg.com:1337&tr=udp://tracker.leechers-paradise.org:6969`;
 
         const xhr = new XMLHttpRequest();
         xhr.withCredentials = true;
